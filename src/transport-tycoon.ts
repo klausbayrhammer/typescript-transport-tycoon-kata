2f3b62adee7f4f4892d1f78d994fba0f52efaadf
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-02-08 00:14:27 +0100

    Removes global world state

--- src/transport-tycoon.ts
+++ src/transport-tycoon.ts
@@ -19,19 +19,16 @@ type TransportTycoonEvent = {
 }
 
 class EventStore {
-    private world = {
-        shipAvailableAt: 0
-    };
-    private upcomingEvents: TransportTycoonEvent[] = [];
+    private allEvents: TransportTycoonEvent[] = [];
     private processedEvents: TransportTycoonEvent[] = [];
 
     public addEvent(type: EventType, time: number) {
-        this.upcomingEvents.push({type, time})
+        this.allEvents.push({type, time})
     }
 
     public simulate() {
         for (let time: number = 0; !this.allOrdersDelivered(); time++) {
-            this.upcomingEvents
+            this.allEvents
                 .filter(event => event.time === time)
                 .forEach(event => {
                     this.on(event);
@@ -46,12 +43,12 @@ class EventStore {
     on(event: TransportTycoonEvent) {
         switch (event.type) {
             case EventType.PickUpOrderAtFactory:
-                const anotherOrderAvailableForPickup = this.nextFactoryOrder();
-                if(anotherOrderAvailableForPickup) {
-                    if (anotherOrderAvailableForPickup.type === EventType.ProduceOrderB) {
+                const nextFactoryOrder = this.nextFactoryOrder();
+                if(nextFactoryOrder) {
+                    if (nextFactoryOrder.type === EventType.ProduceOrderB) {
                         this.addEvent(EventType.OrderDeliveredAtDestinationB, event.time + 5);
                         this.addEvent(EventType.PickUpOrderAtFactory, event.time + 5 * 2);
-                    } else if (anotherOrderAvailableForPickup.type === EventType.ProduceOrderA) {
+                    } else if (nextFactoryOrder.type === EventType.ProduceOrderA) {
                         this.addEvent(EventType.OrderDeliveredAtPort, event.time + 1);
                         this.addEvent(EventType.PickUpOrderAtFactory, event.time + 1 * 2);
                     }
@@ -61,7 +58,6 @@ class EventStore {
                 if(this.isShipAvailable(event)) {
                     this.addEvent(EventType.OrderDeliveredAtDestinationA, event.time + 4);
                     this.addEvent(EventType.PickUpOrderAtPort, event.time + 4 * 2);
-                    this.world.shipAvailableAt = event.time + 4 * 2;
                 }
                 break;
             }
@@ -69,14 +65,13 @@ class EventStore {
                 if(this.isOrderAtPortAvailable()) {
                     this.addEvent(EventType.OrderDeliveredAtDestinationA, event.time + 4);
                     this.addEvent(EventType.PickUpOrderAtPort, event.time + 4 * 2);
-                    this.world.shipAvailableAt = event.time + 4 * 2;
                 }
             }
         }
     }
 
     private isShipAvailable(event: TransportTycoonEvent) {
-        return this.world.shipAvailableAt <= event.time;
+        return !this.allEvents.some(({type, time}) => type === EventType.PickUpOrderAtPort && time > event.time);
     }
 
     private isOrderAtPortAvailable(): boolean {
@@ -86,14 +81,15 @@ class EventStore {
     }
 
     private allOrdersDelivered(): boolean {
-        const ordersProduced = this.upcomingEvents.filter(({type}) => type === EventType.ProduceOrderA || type === EventType.ProduceOrderB).length;
+        const ordersProduced = this.allEvents.filter(({type}) => type === EventType.ProduceOrderA || type === EventType.ProduceOrderB).length;
         const ordersDelivered = this.processedEvents.filter(({type}) => type === EventType.OrderDeliveredAtDestinationA || type === EventType.OrderDeliveredAtDestinationB ).length;
         return ordersProduced === ordersDelivered;
     }
 
     private nextFactoryOrder(): TransportTycoonEvent | undefined {
-        const ordersPickedUp = this.processedEvents.filter(({type}) => type === EventType.PickUpOrderAtFactory).length;
-        return this.processedEvents.filter(({type}) => type === EventType.ProduceOrderA || type === EventType.ProduceOrderB)[ordersPickedUp]
+        const numberOfOrdersPickedUp = this.processedEvents.filter(({type}) => type === EventType.PickUpOrderAtFactory).length;
+        const ordersAtFactory = this.allEvents.filter(({type}) => type === EventType.ProduceOrderA || type === EventType.ProduceOrderB);
+        return ordersAtFactory[numberOfOrdersPickedUp]
     }
 }
 
