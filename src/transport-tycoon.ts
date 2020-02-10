436cebb425733f6530b30c59e068d98f7d3b1b79
Klaus Bayrhammer <klaus.bayrhammer@redbull.com>
2020-02-10 17:32:23 +0100

    refactoring extracts separate event-handlers

--- src/transport-tycoon.ts
+++ src/transport-tycoon.ts
@@ -4,20 +4,20 @@ export enum Destination {
 }
 
 enum EventType {
-    PickUpOrderAtFactory = 'PickUpOrderAtFactory',
-    PickUpOrderAtPort = 'PickUpOrderAtPort',
-    OrderDeliveredAtPort = 'OrderDeliveredAtPort',
-    OrderDeliveredAtDestination = 'OrderDeliveredAtDestination'
+    PickUpOrderAtFactory,
+    PickUpOrderAtPort,
+    OrderDeliveredAtPort,
+    OrderDeliveredAtDestination
 }
-
-const deliveryTimeToPort = 1;
-const deliveryTimeToDestinationA = 4;
-const deliveryTimeToDestinationB = 5;
 
 type TransportTycoonEvent = {
     type: EventType;
     time: number;
 }
+
+const deliveryTimeToPort = 1;
+const deliveryTimeToDestinationA = 4;
+const deliveryTimeToDestinationB = 5;
 
 class TransportTycoon {
     constructor(orders: Destination[]) {
@@ -46,55 +46,62 @@ class TransportTycoon {
         this.events.push({type, time})
     }
 
+    private eventHandlers: Record<EventType, (TransportTycoonEvent?) => void> = {
+        [EventType.PickUpOrderAtFactory]: event => this.pickUpOrderAtFactory(event),
+        [EventType.OrderDeliveredAtDestination]: () => this.orderDeliveredAtDestination(),
+        [EventType.PickUpOrderAtPort]: event => this.pickUpOrderAtPort(event),
+        [EventType.OrderDeliveredAtPort]: event => this.orderDeliveredAtPort(event)
+    };
+
     public simulate() {
         for(let time:number = 0; ; time++){
             this.events
                 .filter(event => event.time === time)
                 .forEach(event => {
-                    this.on(event);
+                    const eventHandler = this.eventHandlers[event.type];
+                    eventHandler && eventHandler(event);
                 });
 
-            const allOrdersDelivered = this.state.numberOfOrders === this.state.ordersDelivered;
-            if(allOrdersDelivered) {
+            if(this.state.numberOfOrders === this.state.ordersDelivered) {
                 return time;
             }
         }
     }
 
-    on(event: TransportTycoonEvent) {
-        switch (event.type) {
-            case EventType.PickUpOrderAtFactory:
-                if (this.state.orders.length > 0) {
-                    const nextOrder = this.state.orders.shift();
-                    if (nextOrder === Destination.B) {
-                        this.addEvent(EventType.OrderDeliveredAtDestination, event.time + deliveryTimeToDestinationB);
-                        this.addEvent(EventType.PickUpOrderAtFactory, event.time + deliveryTimeToDestinationB * 2);
-                    } else {
-                        this.addEvent(EventType.OrderDeliveredAtPort, event.time + deliveryTimeToPort);
-                        this.addEvent(EventType.PickUpOrderAtFactory, event.time + deliveryTimeToPort * 2);
-                    }
-                }
-                break;
-            case EventType.OrderDeliveredAtPort:
-                if (this.state.shipAvailableAtPort) {
-                    this.addEvent(EventType.OrderDeliveredAtDestination, event.time + deliveryTimeToDestinationA);
-                    this.addEvent(EventType.PickUpOrderAtPort, event.time + deliveryTimeToDestinationA * 2);
-                    this.state.shipAvailableAtPort = false;
-                } else {
-                    this.state.ordersAvailableAtPort++;
-                }
-                break;
-            case EventType.PickUpOrderAtPort:
-                if (this.state.ordersAvailableAtPort > 0) {
-                    this.addEvent(EventType.OrderDeliveredAtDestination, event.time + 4);
-                    this.addEvent(EventType.PickUpOrderAtPort, event.time + 4 * 2);
-                    this.state.ordersAvailableAtPort--;
-                } else {
-                    this.state.shipAvailableAtPort = true;
-                }
-                break;
-            case EventType.OrderDeliveredAtDestination:
-                this.state.ordersDelivered++;
+    private orderDeliveredAtDestination() {
+        this.state.ordersDelivered++;
+    }
+
+    private pickUpOrderAtPort(event: TransportTycoonEvent) {
+        if (this.state.ordersAvailableAtPort > 0) {
+            this.addEvent(EventType.OrderDeliveredAtDestination, event.time + 4);
+            this.addEvent(EventType.PickUpOrderAtPort, event.time + 4 * 2);
+            this.state.ordersAvailableAtPort--;
+        } else {
+            this.state.shipAvailableAtPort = true;
+        }
+    }
+
+    private orderDeliveredAtPort(event: TransportTycoonEvent) {
+        if (this.state.shipAvailableAtPort) {
+            this.addEvent(EventType.OrderDeliveredAtDestination, event.time + deliveryTimeToDestinationA);
+            this.addEvent(EventType.PickUpOrderAtPort, event.time + deliveryTimeToDestinationA * 2);
+            this.state.shipAvailableAtPort = false;
+        } else {
+            this.state.ordersAvailableAtPort++;
+        }
+    }
+
+    private pickUpOrderAtFactory(event: TransportTycoonEvent) {
+        if (this.state.orders.length > 0) {
+            const nextOrder = this.state.orders.shift();
+            if (nextOrder === Destination.B) {
+                this.addEvent(EventType.OrderDeliveredAtDestination, event.time + deliveryTimeToDestinationB);
+                this.addEvent(EventType.PickUpOrderAtFactory, event.time + deliveryTimeToDestinationB * 2);
+            } else {
+                this.addEvent(EventType.OrderDeliveredAtPort, event.time + deliveryTimeToPort);
+                this.addEvent(EventType.PickUpOrderAtFactory, event.time + deliveryTimeToPort * 2);
+            }
         }
     }
 }
